f5e5bb6ed608ccf04095711a5faadbd59267988b
Marcel Ruiz Forns <marcel@chaordicsystems.com>
2014-04-04 19:25:06 -0300

    improved code (more simple)

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var SymbolTest = require("./SymbolTest");
 
-SymbolTest.forEach(function (test) { test(); });
+function testSuite (Suite) {
+    Suite.forEach(function (test) {
+        test();
+    });
+}
+
+testSuite(require("./SymbolTest"));
