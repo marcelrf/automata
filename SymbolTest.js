63f3a92db087b50dd8c2398a6efe0747a8067c4f
Marcel Ruiz Forns <marcel@chaordicsystems.com>
2014-04-04 19:24:46 -0300

    removed useless messages and added some basic type tests

--- SymbolTest.js
+++ SymbolTest.js
@@ -9,31 +9,97 @@ module.exports = [
 
     function testParsing () {
         var automaton = newSymbolAutomaton();
-        assert(automaton.state === "parsing", "not parsing after init");
+        assert(automaton.state === "parsing");
     },
 
     function testUndefined () {
         var automaton = newSymbolAutomaton(void 0);
         automaton.parse(void 0);
-        assert(automaton.state === "accepting", "not accepting after undefined symbol");
+        assert(automaton.state === "accepting");
     },
 
     function testUndefinedNegative () {
         var automaton = newSymbolAutomaton(void 0);
         automaton.parse("something");
-        assert(automaton.state === "stopped", "not stopped after non-undefined symbol");
+        assert(automaton.state === "stopped");
     },
 
     function testNull () {
         var automaton = newSymbolAutomaton(null);
         automaton.parse(null);
-        assert(automaton.state === "accepting", "not accepting after null symbol");
+        assert(automaton.state === "accepting");
     },
 
     function testNullNegative () {
         var automaton = newSymbolAutomaton(null);
         automaton.parse(void 0);
-        assert(automaton.state === "stopped", "not stopped after non-null symbol");
+        assert(automaton.state === "stopped");
+    },
+
+    function testBoolean () {
+        var automaton = newSymbolAutomaton(true);
+        automaton.parse(true);
+        assert(automaton.state === "accepting");
+    },
+
+    function testBooleanNegative () {
+        var automaton = newSymbolAutomaton(true);
+        automaton.parse(false);
+        assert(automaton.state === "stopped");
+    },
+
+    function testBooleanNotEquivalent () {
+        var automaton = newSymbolAutomaton(true);
+        automaton.parse(1);
+        assert(automaton.state === "stopped");
+    },
+
+    function testNumber () {
+        var automaton = newSymbolAutomaton(1);
+        automaton.parse(1);
+        assert(automaton.state === "accepting");
+    },
+
+    function testNumberNegative () {
+        var automaton = newSymbolAutomaton(1);
+        automaton.parse(-1);
+        assert(automaton.state === "stopped");
+    },
+
+    function testString () {
+        var automaton = newSymbolAutomaton("symbol");
+        automaton.parse("symbol");
+        assert(automaton.state === "accepting");
+    },
+
+    function testStringNegative () {
+        var automaton = newSymbolAutomaton("symbol");
+        automaton.parse("wrong");
+        assert(automaton.state === "stopped");
+    },
+
+    function testRegExp () {
+        var automaton = newSymbolAutomaton(/a.c/);
+        automaton.parse("abc");
+        assert(automaton.state === "accepting");
+    },
+
+    function testRegExpNegative () {
+        var automaton = newSymbolAutomaton(/a.c/);
+        automaton.parse("wrong");
+        assert(automaton.state === "stopped");
+    },
+
+    function testDate () {
+        var automaton = newSymbolAutomaton(new Date(2000, 0, 1));
+        automaton.parse(new Date(2000, 0, 1));
+        assert(automaton.state === "accepting");
+    },
+
+    function testDateNegative () {
+        var automaton = newSymbolAutomaton(new Date(2000, 0, 1));
+        automaton.parse(new Date(2000, 0, 2));
+        assert(automaton.state === "stopped");
     },
 
     function testFunction () {
@@ -41,7 +107,7 @@ module.exports = [
             return symbol === 1;
         });
         automaton.parse(1);
-        assert(automaton.state === "accepting", "not accepting after expected symbol");
+        assert(automaton.state === "accepting");
     },
 
     function testFunctionNegative () {
@@ -49,33 +115,75 @@ module.exports = [
             return symbol === 1;
         });
         automaton.parse(2);
-        assert(automaton.state === "stopped", "not stopped after unexpected symbol");
+        assert(automaton.state === "stopped");
     },
 
-    function testObject1 () {
+    function testObject () {
         var automaton = newSymbolAutomaton({property: true});
-        automaton.parse({property: true, other: false});
-        assert(automaton.state === "accepting", "not accepting after expected symbol");
+        automaton.parse({property: true});
+        assert(automaton.state === "accepting");
     },
 
-    function testObject2 () {
+    function testObjectNegative () {
+        var automaton = newSymbolAutomaton({property: true});
+        automaton.parse({property: false});
+        assert(automaton.state === "stopped");
+    },
+
+    function testObjectUndefined () {
+        var automaton = newSymbolAutomaton({property: true});
+        automaton.parse({});
+        assert(automaton.state === "stopped");
+    },
+
+    function testObjectRecursive () {
         var automaton = newSymbolAutomaton({property1: {property2: true}});
         automaton.parse({property1: {property2: true, other2: false}, other1: false});
-        assert(automaton.state === "accepting", "not accepting after expected symbol");
+        assert(automaton.state === "accepting");
+    },
+
+    function testArray () {
+        var automaton = newSymbolAutomaton([1, 2, 3]);
+        automaton.parse([1, 2, 3]);
+        assert(automaton.state === "accepting");
+    },
+
+    function testArrayNegativeLess () {
+        var automaton = newSymbolAutomaton([1, 2, 3]);
+        automaton.parse([1, 2]);
+        assert(automaton.state === "stopped");
+    },
+
+    function testArrayNegativeMore () {
+        var automaton = newSymbolAutomaton([1, 2, 3]);
+        automaton.parse([1, 2, 3, 4]);
+        assert(automaton.state === "stopped");
+    },
+
+    function testArrayRecursive () {
+        var automaton = newSymbolAutomaton([[1, 2], [3, 4]]);
+        automaton.parse([[1, 2], [3, 4]]);
+        assert(automaton.state === "accepting");
+    },
+
+    function testArrayAndObjectRecursive () {
+        var automaton = newSymbolAutomaton([{property: [1, 2, 3]}, [{property: false}, 4], 5]);
+        automaton.parse([{property: [1, 2, 3], other: false}, [{property: false, other: true}, 4], 5]);
+        assert(automaton.state === "accepting");
     },
 
     function testStoppedAfterAccept () {
         var automaton = newSymbolAutomaton("symbol");
         automaton.parse("symbol");
         automaton.parse("symbol");
-        assert(automaton.state === "stopped", "not stopped after accepting and receiving another symbol");
+        assert(automaton.state === "stopped");
     },
 
     function testStoppedAfterStopped () {
         var automaton = newSymbolAutomaton("symbol");
         automaton.parse("wrong");
         automaton.parse("symbol");
-        assert(automaton.state === "stopped", "not stopped after stopped and receiving another symbol");
+        assert(automaton.state === "stopped");
     }
 
 ];
